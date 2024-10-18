ab776a359f04cb2cbb4ed468ff2410aeb67b6107
masyumero <167562220+masyumero@users.noreply.github.com>
2024-10-18 09:18:43 +0900

    creative tab

--- src/main/java/com/fxd927/mekanismscience/common/MSCreativeTab.java
+++ src/main/java/com/fxd927/mekanismscience/common/MSCreativeTab.java
@@ -5,20 +5,6 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-//public class MSCreativeTab {
-//    public static final CreativeTabDeferredRegister CREATIVE_TABS = new CreativeTabDeferredRegister(MekanismScience.MODID);
-//
-//    public static final CreativeTabRegistryObject MEKANISM_SCIENCE = CREATIVE_TABS.registerMain(MSLang.MEKANISM_SCIENCE, MSItems.NEUTRON_SOURCE_PELLET, builder ->
-//              builder.withBackgroundLocation(MekanismScience.rl("textures/gui/creative_tab.png"))
-//                      .withSearchBar(70)
-//                      .withTabsBefore(MekanismCreativeTabs.MEKANISM.key())
-//                      .displayItems((displayParameters, output) -> {
-//                          CreativeTabDeferredRegister.addToDisplay(MSItems.ITEMS, output);
-//                          //CreativeTabDeferredRegister.addToDisplay(MSBlocks.BLOCKS, output);
-//                          CreativeTabDeferredRegister.addToDisplay(MSFluids.FLUIDS, output);
-//                    })
-//    );
-//}
 public class MSCreativeTab {
 
     // 新しいクリエイティブタブの定義
