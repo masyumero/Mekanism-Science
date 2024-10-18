ff659bfa729d4d6a80b03b897e4750aee9634888
masyumero <167562220+masyumero@users.noreply.github.com>
2024-10-18 09:41:01 +0900

    creative tab

--- src/main/java/com/fxd927/mekanismscience/common/MSCreativeTab.java
+++ src/main/java/com/fxd927/mekanismscience/common/MSCreativeTab.java
@@ -1,6 +1,8 @@
 package com.fxd927.mekanismscience.common;
 
+import com.fxd927.mekanismscience.common.registries.MSFluids;
 import com.fxd927.mekanismscience.common.registries.MSItems;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -13,6 +15,20 @@ public class MSCreativeTab {
         public @NotNull ItemStack makeIcon() {
             // クリエイティブタブのアイコンに使用するアイテムを設定（例：ダイヤモンド）
             return new ItemStack(MSItems.NEUTRON_SOURCE_PELLET);
+        }
+        @Override
+        public void fillItemList(NonNullList<ItemStack> items) {
+            super.fillItemList(items);
+
+            // カスタムタブに表示させたいアイテムを一括で追加
+            items.add(new ItemStack(MSItems.NEUTRON_SOURCE_PELLET));
+            items.add(new ItemStack(MSItems.DUST_CALCIUM_OXIDE));
+            items.add(new ItemStack(MSFluids.YTTRIUM.getBucket()));
+            items.add(new ItemStack(MSFluids.BERYLLIUM.getBucket()));
+            items.add(new ItemStack(MSFluids.HELIUM.getBucket()));
+            items.add(new ItemStack(MSFluids.STRONTIUM.getBucket()));
+            items.add(new ItemStack(MSFluids.SUPERHEATED_HELIUM.getBucket()));
+
         }
     };
 }
--- src/main/java/com/fxd927/mekanismscience/common/registries/MSItems.java
+++ src/main/java/com/fxd927/mekanismscience/common/registries/MSItems.java
@@ -13,9 +13,9 @@ import net.minecraft.world.item.Item;
 public class MSItems {
     public static final ItemDeferredRegister ITEMS = new ItemDeferredRegister(MekanismScience.MODID);
 
-    public static final ItemRegistryObject<Item> NEUTRON_SOURCE_PELLET = ITEMS.register("pellet_neutron_source", () -> new NeutronSourcePellet(new Item.Properties().tab(MSCreativeTab.MEKANISM_SCIENCE), EnumColor.YELLOW));
+    public static final ItemRegistryObject<Item> NEUTRON_SOURCE_PELLET = ITEMS.register("pellet_neutron_source", () -> new NeutronSourcePellet(new Item.Properties(), EnumColor.YELLOW));
     //public static final ItemRegistryObject<Item> HIGH_DENSITY_NEUTRON_SOURCE_PELLET = ITEMS.register("pellet_high-density_neutron_source", () -> new HighDensityNeutronSourcePellet(new Item.Properties(), EnumColor.PURPLE));
-    public static final ItemRegistryObject<Item> DUST_CALCIUM_OXIDE = ITEMS.register("dust_calcium_oxide", () -> new Item(new Item.Properties().tab(MSCreativeTab.MEKANISM_SCIENCE)));
+    public static final ItemRegistryObject<Item> DUST_CALCIUM_OXIDE = ITEMS.register("dust_calcium_oxide");
     //public static final ItemRegistryObject<Item> DUST_YTTRIUM = ITEMS.register("dust_yttrium");
     //public static final ItemRegistryObject<Item> REFINED_CALIFORNIUM_INGOT = ITEMS.register("ingot_refined_californium", EnumColor.ORANGE);
     //public static final ItemRegistryObject<Item> EXCIPIENT = ITEMS.register("excipient");
